8769a0011d172f77d0bb4a881979922beee17193
Linh Vu <linhgb+github@gmail.com>
2022-11-15 15:22:43 +1100

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "vucslab"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
